26787f603f9c913594b2e6a4bb8ebb0e7ed19a51
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-04-20 15:50:17 -0300

    Initial commit

---
+++ src/main/java/com/letscode/hogwartsRegistration/HogwartsRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.hogwartsRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/letscode/hogwartsRegistration/HogwartsRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.hogwartsRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
