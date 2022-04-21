5672cc32f1b624204edd74831b5d3581c16f1cea
Marisa Sales <73047662+marisasales@users.noreply.github.com>
2022-04-21 02:16:40 -0300

    refactor: enable jpa auditing and add open api definition

--- src/main/java/com/letscode/hogwartsRegistration/HogwartsRegistrationApplication.java
+++ src/main/java/com/letscode/hogwartsRegistration/HogwartsRegistrationApplication.java
@@ -1,9 +1,18 @@
 package com.letscode.hogwartsRegistration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Hogwarts Student Registration",
+        version = "1.0",
+        description = "API for registering wizard students at Hogwarts School of Witchcraft and Wizardry"))
 public class HogwartsRegistrationApplication {
 
 	public static void main(String[] args) {
